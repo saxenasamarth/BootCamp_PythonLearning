717cc9eb0b1fd2cda80daf94bc24131fe77f6562
Samarth <saxenasamarth@gmail.com>
2019-07-14 16:12:58 +0530

    miniAmazonFiles

--- MiniAmazon/api.py
+++ MiniAmazon/api.py
@@ -7,7 +7,8 @@ app.secret_key = 'hello'
 
 @app.route("/")
 def home_page():
-	session.clear()
+	#session.clear()
+	#print("logged in as {0} and {1}".format(str(session["username"]),str(session["type"])))
 	return render_template("home.html",title ="home")
 
 @app.route("/about")
@@ -18,9 +19,52 @@ def about_page():
 def contact_page():
 	return render_template("contact.html")
 
+@app.route("/login", methods=["POST"])
+def login_page():
+	uname=request.form["username"]
+	passw=request.form["password"]
+
+	if uname_exists(uname):
+		checkpass_resp=check_passw(uname, passw)
+		if checkpass_resp!=None:
+			session["username"]=uname
+			session["type"]=checkpass_resp
+			#print("logged in as {0} and {1}".format(str(session["username"]),str(session["type"])))
+			return redirect(url_for("home_page"))
+		else:
+			return "username password not valid"
+	else:
+		return "username does not exist"
+
+@app.route("/products",methods=["POST","GET"])
+def product_page():
+	if request.method=="POST":
+		prod_name = request.form["name"]
+		prod_price=request.form["price"]
+		prod_desc=request.form["description"]
+		#isProductAdded = add_ProdToList(prod_name,prod_price,prod_desc)
+		if add_ProdToList(prod_name,prod_price,prod_desc,session["username"]) == True:
+			return redirect(url_for("home_page"))
+		else:
+			return "Product Exists in Database, Enter New Product"
+	else :
+		return render_template("products.html",products=getAllProducts())
+
+@app.route("/remove_product", methods=["POST"])
+def removeProduct_page():
+	if request.method=="POST":
+		prod_name=request.form["name"]
+		deleteProduct(prod_name)
+		return redirect(url_for("product_page"))
+
+
+
+
+
+
 @app.route("/signup", methods=['POST'])
 def signup_page():
-	print(request.form)
+	#print(request.form)
 	uname=request.form["username"]
 	passw=request.form["password1"]
 	veri_passw=request.form["password2"]
@@ -34,9 +78,34 @@ def signup_page():
 			userProfile["passw"]=passw
 			userProfile["type"]=user_type
 			createUser(userProfile)
+			session["username"]=uname
+			session["type"]=user_type
 			return redirect(url_for("home_page"))
 		else:
 			return "passwords do not match"
 
+
+@app.route("/cart", methods=['POST','GET'])
+def addtocart_page():
+	cart=[]
+	if request.method=="POST":
+		product_id=request.form["id"]
+		cart_items = addtoCart(product_id, session["username"])
+		for keyofItem in cart_items:
+			cart.append(getProductInfo(keyofItem))
+			#print(cart)
+		return render_template("cart.html", cart=cart)
+	else:
+
+		cart_items=getCart(session["username"])
+		for keyofItem in cart_items:
+			cart.append(getProductInfo(keyofItem))
+			#print(cart)
+		return render_template("cart.html", cart=cart)
+
+@app.route("/logout", methods=['GET'])
+def logout_page():
+	session.clear()
+	return "logged out, Close window, open and login again"
 
 app.run(debug=True)
--- MiniAmazon/models/model.py
+++ MiniAmazon/models/model.py
@@ -1,17 +1,82 @@
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 client=MongoClient()
 db=client["MiniAmazon"]
 
 
-def uname_exists(uname_val):
-	if db.users.find_one({"username":uname_val})!=None:
+def uname_exists(uname):
+	if db.users.find_one({"username":uname})!=None:
 		return True
 	else:
 		return False
 
+def check_passw(uname, password):
+	user_detail=db.users.find_one({"username":uname}) # as we want to use the response of this find on DB multiple time, we have saved in variable
+	if user_detail!=None:
+		if user_detail["passw"]==password:
+			return user_detail["type"]
+		else:
+			return "Wrong password"
+
 
 def createUser(userProfile_dict):
 	db.users.insert(userProfile_dict)
+
+
+#product related function
+
+def add_ProdToList(pname,pprice,pdesc,uname):
+	if db["Products"].find_one({"product_name":pname})==None:
+		db["Products"].insert({"product_name":pname,"product_price":pprice,"product_desc":pdesc,"product_seller":uname})
+		return True
+	else:
+		return False
+
+def getAllProducts():
+	all_products=db["Products"].find({})
+	prod_list=[]
+	for x in all_products:
+		prod_list.append(x)
+	return prod_list
+
+def deleteProduct(product_name):
+	db["Products"].remove({"product_name":product_name})
+
+def getProductInfo(item_id):
+	itemdetails_list=[]
+	print(item_id)
+	item_detail=db["Products"].find_one({"_id":ObjectId(item_id)})
+	print(item_detail)
+	return item_detail
+
+
+
+#cart functions
+def addtoCart(p_id,username):
+	userInfo=db["users"].find_one({"username":username})
+	if userInfo!=None:
+		if "cart" in userInfo:
+			cart_dict=userInfo["cart"]
+		else:
+			cart_dict={}
+	print(cart_dict)
+
+	if p_id in cart_dict:
+		cart_dict[p_id]+=1
+		db["users"].update({"username":username},{'$set':{"cart":cart_dict}})
+		mycart=db["users"].find_one({"username":username})
+		return mycart["cart"]
+
+	else:
+		cart_dict[p_id]=1
+		db["users"].update({"username":username},{'$set':{"cart":cart_dict}})
+		mycart=db["users"].find_one({"username":username})
+		return mycart["cart"]
+
+def getCart(username):
+	mycart=db["users"].find_one({"username":username})
+	return mycart["cart"]
+
 
 
